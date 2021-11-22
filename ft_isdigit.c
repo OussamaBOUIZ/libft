610d2e5c76174c5253c056dd4194735aa70253e3
Oussama Bouizgarne <obouizga@e1r13p7.1337.ma>
2021-11-22 13:54:34 +0100

    blabla

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: obouizga <obouizga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 15:41:17 by obouizga          #+#    #+#             */
-/*   Updated: 2021/11/22 10:53:45 by obouizga         ###   ########.fr       */
+/*   Updated: 2021/11/22 13:17:08 by obouizga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	ft_isdigit(int c)
 {
-	char	cc;
+	unsigned char	cc;
 
-	cc = (char)c;
+	cc = (unsigned char)c;
 	if (cc >= '0' && cc <= '9')
 		return (1);
 	return (0);
--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: obouizga <obouizga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 18:40:13 by obouizga          #+#    #+#             */
-/*   Updated: 2021/11/16 09:23:46 by obouizga         ###   ########.fr       */
+/*   Updated: 2021/11/22 13:20:44 by obouizga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_memset(void *b, int c, size_t len)
 	i = 0;
 	while (i < len)
 	{
-		*((unsigned char *)b + i) = c;
+		*((unsigned char *)b + i) = (unsigned char)c;
 		i++;
 	}
 	return (b);
